c5dd0a56ef2499ed40af0092de5db33efc7ebb3e
Ragvalod <konfucy@gmail.com>
2023-08-20 13:57:13 +0300

    20.08.2023 Добавлен магазин и Бонусы

--- Assets/Scripts/GamePlay.cs
+++ Assets/Scripts/GamePlay.cs
@@ -1,18 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class GamePlay : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private int score;
+    public TextMeshPro textScore;
+
+    public GameObject shopPanel, bonusPanel, mainCamera, cameraRotation;
+    
+
+    public void Start()
     {
-        
+        score = PlayerPrefs.GetInt("money");
     }
 
-    // Update is called once per frame
-    void Update()
+    //Метод обрабатываемый нажатие кропки по экрану (для увеличения количества денег)
+    public void onClickBtnScore()
+    {
+        score++;
+        //textScore.text = score + "$";
+        PlayerPrefs.SetInt("money", score);
+    }
+
+    //Обработчик нажатия кнопки для вызова панели магазина
+    public void onClikcBtnShop()
+    {
+        zoomToZofObject(500);
+        shopPanel.SetActive(!shopPanel.activeSelf);
+    }
+    //Обработчик нажатия кнопки для вызова панели бонусов
+    public void onClikcBtnBonus()
+    {
+        zoomToZofObject(500);
+        bonusPanel.SetActive(!bonusPanel.activeSelf);
+    }
+
+    //Обработчик нажатия кнопки для вызова панели бонусов
+    public void onClikcCloseShopOrBonus()
+    {
+        zoomToZofObject(120);
+        bonusPanel.SetActive(false);
+        shopPanel.SetActive(false);
+    }
+
+    //Метод отдаления от обьекта по оси Z
+    private void zoomToZofObject(long distans)
     {
         
+        mainCamera.transform.position = cameraRotation.transform.position;
+        mainCamera.transform.Translate(0, 0, -distans);
+    }
+
+    private void Update()
+    {
+        textScore.text = score + "$";
     }
 }
+
